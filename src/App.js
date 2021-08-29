8d2da44753437a35626ef78ea9f954aa0474f120
naiem <naiem4288@gamil.com>
2021-08-29 22:22:01 +0600

    pass hardcorad data from component and recives data using props

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import Person from './Person/Person';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hi, I'm React App</h1>
+      <p>This is really working!</p>
+      <button>Switch Name</button>
+      <Person name="Harry Potter" age="100"/>
+      <Person name="Tony Stack" age="200">My Hobbies: Racing</Person>
+      <Person name="Pitter Parker" age="300"/>
     </div>
   );
 }
---
+++ src/Person/Person.js
@@ -0,0 +1,11 @@
+
+const Person = props => {
+  return (
+    <div>
+      <p> I'm a {props.name} and I am {props.age} years old!</p>
+      <p>{ props.children }</p>
+    </div>
+  );
+};
+
+export default Person;
