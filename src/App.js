33a48ff05a767634fff1a2c08ddd7159b44c603e
naiem <naiem4288@gamil.com>
2021-08-30 16:26:21 +0600

    conditional list render using if else

--- src/App.js
+++ src/App.js
@@ -55,7 +55,35 @@ class App extends Component {
       padding: '8px',
       cursor: 'pointer'
     }
+
+    let persons = null;
     
+    // Recommended way to conditional render
+    if (this.state.showPersons) {
+      persons = (
+        <div>
+          <Person
+            name={this.state.persons[0].name}
+            age={this.state.persons[0].age}
+          />
+          <Person
+            name={this.state.persons[1].name}
+            age={this.state.persons[1].age}
+            click={this.switchNameHandler.bind(this, 'Rogers!')}
+            changed={this.nameChangeHandler}
+          >
+            My Hobbies: Racing
+          </Person>
+          <Person
+            name={this.state.persons[2].name}
+            age={this.state.persons[2].age}
+          />
+        </div>
+      );
+    };
+    
+    console.log(persons);
+
     return (
       <div className="App">
         <h1>Hi, I'm React App</h1>
@@ -64,27 +92,9 @@ class App extends Component {
           style={style}
           onClick={this.togglePersonHandler}
         >Toggle Persons</button>
-        {
-          this.state.showPersons === true ?
-            <div>
-              <Person
-                name={this.state.persons[0].name}
-                age={this.state.persons[0].age}
-              />
-              <Person
-                name={this.state.persons[1].name}
-                age={this.state.persons[1].age}
-                click={this.switchNameHandler.bind(this, 'Rogers!')}
-                changed={this.nameChangeHandler}
-              >
-                My Hobbies: Racing
-              </Person>
-              <Person
-                name={this.state.persons[2].name}
-                age={this.state.persons[2].age}
-              />
-            </div> : null
-        }
+
+        { persons}
+        
       </div>
     );
   }
